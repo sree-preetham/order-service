e6ff0cd6b2b66f2b6bb747ee14b5f19f5eeefb94
sree-preetham <preethamsree886@gmail.com>
2025-06-11 20:47:47 +0900

    Place Order request

---
+++ src/main/java/com/example/order_service/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.example.order_service.controller;
+
+import com.example.order_service.dto.OrderDto;
+import com.example.order_service.form.OrderRequestForm;
+import com.example.order_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/orders")
+public class OrderController {
+    private final OrderService orderService;
+    @PostMapping("")
+    ResponseEntity<OrderDto> placeOrder(@RequestBody OrderRequestForm form){
+        OrderDto orderDto = orderService.placeOrder(form);
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderDto);
+    }
+}
---
+++ src/main/java/com/example/order_service/dto/OrderDto.java
@@ -0,0 +1,24 @@
+package com.example.order_service.dto;
+
+import com.example.order_service.entity.Order;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDto {
+    private UUID id;
+    private String orderNumber;
+    private List<OrderLineItemsDto> orderLineItemsList;
+
+    public OrderDto(Order order, List<OrderLineItemsDto> orderLineItemsDtoList) {
+        this.id = order.getId();
+        this.orderNumber = order.getOrderNumber();
+        this.orderLineItemsList = orderLineItemsDtoList;
+    }
+}
