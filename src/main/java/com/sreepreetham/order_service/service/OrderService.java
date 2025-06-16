9bbcc2f4e9e9c8fc15477e554176289935952c22
sree-preetham <preethamsree886@gmail.com>
2025-06-16 20:02:01 +0900

    Transactional Service

--- src/main/java/com/sreepreetham/order_service/service/OrderService.java
+++ src/main/java/com/sreepreetham/order_service/service/OrderService.java
@@ -8,12 +8,14 @@ import com.sreepreetham.order_service.form.OrderRequestForm;
 import com.sreepreetham.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
     public OrderDto placeOrder(OrderRequestForm form){
